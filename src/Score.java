cd05fe0a6ab9c60191cc5e74d1162d4ed7165ef9
342348646 <342348646@C-1K2XRD2.tdsb.on.ca>
2025-05-21 11:31:48 -0400

    z

---
+++ src/Score.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 342348646
+ */
+public class Score {
+    private int score;
+    private static final
+}
