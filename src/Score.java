b597e0bc05559a9a86313a451e152294f90316e9
342393436 <342393436@C-G5JWJB2.tdsb.on.ca>
2025-05-23 10:49:16 -0400

    added score

--- src/Score.java
+++ src/Score.java
@@ -9,14 +9,18 @@
  */
 public class Score {
     private int score;
-    private static final int TOTAL_QUESTIONS;
+    private static final int TOTAL_QUESTIONS = 10;
     public void addScore(){
-        
+        if (score < TOTAL_QUESTIONS){
+            score++;
+        }
     }
     public int getFinal(){
         return score;
     }
     public double getPercentage(){
+        double percent = (score / TOTAL_QUESTIONS) * 100;
+        return percent;
         
     } 
 }
